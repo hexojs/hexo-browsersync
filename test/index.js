da97d0c02ee49d58739f670c50f53b2bf3e5bd4f
Tony Crowe <github@tonycrowe.com>
2017-11-20 07:26:46 -0700

    upgrades and testing

---
+++ test/index.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var assert = require('assert');
+
+function noop() {}
+
+describe('hexo-browsersync', function() {
+  var index;
+  var filter;
+  var hexoEvents = {};
+  var bsEvents = {};
+  var bs;
+
+  var mockHexo = {
+    extend: {
+      filter: {
+        register: noop
+      }
+    },
+    config: {},
+    on: function (key, val) {
+      hexoEvents[key] = val;
+    },
+    route: {
+      on: function (key, val) {
+        bsEvents[key] = val;
+      }
+    }
+  };
+
+  var mockApp = {
+    use: noop
+  };
+
+  global.hexo = mockHexo;
+
+  index = require('../index');
+  filter = require('../lib/browsersync').bind(mockHexo);
+
+  it('initializes', function() {
+    filter(mockApp);
+    assert.equal(typeof mockHexo.browsersync, 'object');
+    assert.equal(typeof mockHexo.browsersync.condition, 'function');
+    assert.equal(typeof mockHexo.browsersync.converter, 'function');
+    bs = mockHexo.browsersync;
+  });
+
+  it('browsersync initializes', function(done) {
+    mockHexo.browsersync.emitter.on('init', function() {
+      done();
+    });
+  });
+
+  it('runs the condition', function() {
+    var mockResHtml = {
+      getHeader: function () {
+        return 'text/html';
+      }
+    };
+    var mockResText = {
+      getHeader: function () {
+        return 'text/plain';
+      }
+    };
+    bs.condition(null, mockResHtml);
+    bs.condition(null, mockResText);
+  });
+
+  it('runs the converter', function() {
+    var contentBody = '<html><body></body></html>';
+    var contentNoBody = 'just text';
+    bs.converter(contentBody, null, null, function (err, res) {
+      assert.equal(err, null);
+      assert(res.indexOf('script') > -1);
+    });
+    bs.converter(contentNoBody, null, null, function (err, res) {
+      assert.equal(err, null);
+      assert.equal(res, contentNoBody);
+    });
+  });
+
+  it('fires events', function() {
+    Object.keys(hexoEvents).forEach(function(key) {
+      hexoEvents[key]();
+    });
+    Object.keys(bsEvents).forEach(function(key) {
+      bsEvents[key]();
+    });
+  });
+
+  it('exits', function() {
+    // hexoEvents.exit();
+    mockHexo.browsersync.exit();
+  });
+});
